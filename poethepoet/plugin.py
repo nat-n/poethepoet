c5e0a0b49dfe8ebab9e11757a3e55021ae24a225
Bryan Hu <bryan.hu.2020@gmail.com>
2021-07-03 16:39:16 -0700

    :bug: Should work now

--- poethepoet/plugin.py
+++ poethepoet/plugin.py
@@ -1,33 +1,42 @@
 import shutil
 import subprocess
+from pathlib import Path
 from typing import Iterator
 
 import tomlkit
 from cleo.commands.command import Command
+from poetry.console import application as app
 from poetry.plugins.application_plugin import ApplicationPlugin
 
 from . import config
 
 
 def get_poe_task_names() -> Iterator[str]:
-    yield from tomlkit.loads(config.PoeConfig().find_pyproject_toml())["tool"]["poe"][
-        "tasks"
-    ].keys()
+    return (
+        name
+        for name in tomlkit.loads(
+            Path(config.PoeConfig().find_pyproject_toml()).read_text()
+        )["tool"]["poe"]["tasks"].keys()
+        if name not in app.COMMANDS
+    )
 
 
-def run_poe_task(name: str):
+def run_poe_task(name: str) -> int:
     return subprocess.call([shutil.which("poe"), name])
 
 
 class PoePlugin(ApplicationPlugin):
-    def activate(self, application):
+    def activate(self, application: app.Application) -> None:
         for task_name in get_poe_task_names():  # Assume this function exists somewhere
             application.command_loader.register_factory(
                 task_name,
-                lambda x: type(
-                    task_name.capitalize() + "Command",
-                    (Command),
-                    {"name": task_name, "handle": lambda self: run_poe_task(self.name)},
+                type(
+                    task_name.replace("-", "_").capitalize() + "Command",
+                    (Command,),
+                    {
+                        "name": task_name,
+                        "handle": lambda inner_self: run_poe_task(inner_self.name),
+                    },
                     # Also assume `run_poe_task` accepts task name as an argument, runs that poe task
                     # and returns the return code of the poe task
                 ),
