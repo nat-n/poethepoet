cb17707b2c5d7c742b33aa77bd231cdbf6ff0f5c
Artemis <hollyshort21@gmail.com>
2021-08-24 12:37:18 +0100

    Make the -v and -q flags incremental (#35)
    The -v and -q flags can be provided multiple times to increase/decrease verbosity by an arbitrary amount relative to the starting point.
    
    This makes it is possible to specify normal verbosity, even if a default verbosity is configured, and will make it easier to support more verbosity levels than just -1, 0 and 1 in the future.

--- tests/test_poe_config.py
+++ tests/test_poe_config.py
@@ -33,15 +33,34 @@ def test_setting_global_env_vars(run_poe_subproc, is_windows):
     assert result.stderr == ""
 
 
-def test_setting_default_verbosity(run_poe_subproc, verbosity_default_project_path):
-    result = run_poe_subproc("test", cwd=verbosity_default_project_path,)
+def test_setting_default_verbosity(run_poe_subproc, low_verbosity_project_path):
+    result = run_poe_subproc("test", cwd=low_verbosity_project_path,)
     assert result.capture == ""
     assert result.stdout == "Hello there!\n"
     assert result.stderr == ""
 
 
-def test_override_default_verbosity(run_poe_subproc, verbosity_default_project_path):
-    result = run_poe_subproc("-v", "test", cwd=verbosity_default_project_path,)
+def test_override_default_verbosity(run_poe_subproc, low_verbosity_project_path):
+    result = run_poe_subproc("-v", "-v", "test", cwd=low_verbosity_project_path,)
     assert result.capture == "Poe => echo Hello there!\n"
     assert result.stdout == "Hello there!\n"
     assert result.stderr == ""
+
+
+def test_partially_decrease_verbosity(run_poe_subproc, high_verbosity_project_path):
+    result = run_poe_subproc("-q", "test", cwd=high_verbosity_project_path,)
+    assert result.capture == "Poe => echo Hello there!\n"
+    assert result.stdout == "Hello there!\n"
+    assert result.stderr == ""
+
+
+def test_decrease_verbosity(run_poe_subproc, dummy_project_path, is_windows):
+    result = run_poe_subproc("-q", "part1", cwd=dummy_project_path,)
+    assert result.capture == ""
+    assert result.stderr == ""
+    if is_windows:
+        # On Windows, "echo 'Hello'" results in "'Hello'".
+        assert result.stdout == "'Hello'\n"
+    else:
+        # On UNIX, "echo 'Hello'" results in just "Hello".
+        assert result.stdout == "Hello\n"
