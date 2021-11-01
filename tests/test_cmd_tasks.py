154a65e8eb6f523ea019c10e26da46b933691967
Michael Jaquier <shibasu@gmail.com>
2021-11-01 18:57:39 +0100

    Moved tests to more aptly named files for orgnizational purposes

--- tests/test_cmd_tasks.py
+++ tests/test_cmd_tasks.py
@@ -12,32 +12,6 @@ def test_call_echo_task(run_poe_subproc, projects, esc_prefix):
     assert result.stderr == ""
 
 
-def test_setting_envvar_in_task(run_poe_subproc, projects):
-    # The $ has to be escaped or it'll be evaluated by the outer shell and poe will
-    # never see it
-    result = run_poe_subproc("show_env")
-    assert result.capture == f"Poe => env\n"
-    assert f"POE_ROOT={projects['example']}" in result.stdout
-    assert result.stderr == ""
-
-
-def test_shell_task(run_poe_subproc):
-    result = run_poe_subproc("count")
-    assert (
-        result.capture
-        == f"Poe => echo 1 && echo 2 && echo $(python -c 'print(1 + 2)')\n"
-    )
-    assert result.stdout == "1\n2\n3\n"
-    assert result.stderr == ""
-
-
-def test_shell_task_raises_given_extra_args(run_poe):
-    result = run_poe("count", "bla")
-    assert f"\n\nError: Shell task 'count' does not accept arguments" in result.capture
-    assert result.stdout == ""
-    assert result.stderr == ""
-
-
 def test_ref_task(run_poe_subproc, projects, esc_prefix):
     # This should be exactly the same as calling the echo task directly
     result = run_poe_subproc("also_echo", "foo", "!")
@@ -51,21 +25,12 @@ def test_ref_task(run_poe_subproc, projects, esc_prefix):
     assert result.stderr == ""
 
 
-def test_multiline_non_default_type_task(run_poe_subproc, esc_prefix):
-    # This should be exactly the same as calling the echo task directly
-    result = run_poe_subproc("sing")
-    assert result.capture == (
-        f'Poe => echo "this is the story";\n'
-        'echo "all about how" &&      # the last line won\'t run\n'
-        'echo "my life got flipped;\n'
-        '  turned upside down" ||\necho "bam bam baaam bam"\n'
-    )
-    assert result.stdout == (
-        f"this is the story\n"
-        "all about how\n"
-        "my life got flipped;\n"
-        "  turned upside down\n"
-    )
+def test_setting_envvar_in_task(run_poe_subproc, projects):
+    # The $ has to be escaped or it'll be evaluated by the outer shell and poe will
+    # never see it
+    result = run_poe_subproc("show_env")
+    assert result.capture == f"Poe => env\n"
+    assert f"POE_ROOT={projects['example']}" in result.stdout
     assert result.stderr == ""
 
 
