dcb5f4b3b55511b320c8e9fc87c2076491a097f4
Bryan Hu <bryan.hu.2020@gmail.com>
2021-12-23 21:22:21 -0800

    Some changes

--- poethepoet/plugin.py
+++ poethepoet/plugin.py
@@ -6,22 +6,9 @@ from typing import Iterator
 
 import tomlkit
 
-try:
-    # pylint: disable=import-error
-    from cleo.commands.command import Command
-    from poetry.console import application as app
-    from poetry.plugins.application_plugin import ApplicationPlugin
-except ModuleNotFoundError:
-    # Hacky defaults to
-    # tell whether poetry is installed or not,
-    # please mypy,
-    # and please Poetry's plugin system (simulate the ApplicationPlugin object)
-    app = type("application", (), {"Application": None})  # pylint: disable=C0103
-    ApplicationPlugin = type(
-        "ApplicationPlugin",
-        (),
-        {"PLUGIN_API_VERION": "1.0.0", "type": "application.plugin"},
-    )
+from cleo.commands.command import Command
+from poetry.console import application as app
+from poetry.plugins.application_plugin import ApplicationPlugin
 
 from . import config
 
@@ -38,6 +25,7 @@ def get_poe_task_names() -> Iterator[str]:
 
 
 def run_poe_task(name: str, io) -> int:
+    # Hacks to use Cleo's IO
     io.fileno = sys.stdin.fileno  # Monkey patches io because subprocess needs it
     stdin = io
     io.fileno = sys.stdout.fileno
@@ -56,11 +44,6 @@ def run_poe_task(name: str, io) -> int:
 
 class PoePlugin(ApplicationPlugin):
     def activate(self, application: app.Application) -> None:
-        if app.Application is None:
-            print(
-                "Poetry not found. Did you install this plugin via `poetry plugin add poethepoet[poetry_plugin]`?"
-            )
-            sys.exit(1)
         io = application.create_io()
         # Create seperate commands per task
         for task_name in get_poe_task_names():
