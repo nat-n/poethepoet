16824a74ffed17186467446274b63a870e532508
Nat Noordanus <n@natn.me>
2022-11-12 23:38:40 +0100

    Silence mypy error

--- poethepoet/config.py
+++ poethepoet/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 try:
     import tomllib as tomli
 except ImportError:
-    import tomli
+    import tomli  # type: ignore
 from typing import Any, Dict, Mapping, Optional, Sequence, Tuple, Union
 from .exceptions import PoeException
 
