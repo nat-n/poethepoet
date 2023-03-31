d9e1a527a073aa42a8fc3fd432fc7bd94ffbe785
Samuel Prevost <sam.prevost@zivver.com>
2023-03-31 14:53:46 +0200

    Initialise Sphinx docs directory

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Poe The Poet'
+copyright = '2023, Nat Noordanus <n@natn.me>'
+author = 'Nat Noordanus <n@natn.me>'
+release = '0.19.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. Poe The Poet documentation master file, created by
+   sphinx-quickstart on Fri Mar 31 14:48:14 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Poe The Poet's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
