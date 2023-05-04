81f0d414aec5a23ada641762981d07413e149cf7
Adam Meily <meily.adam@gmail.com>
2023-05-04 18:36:53 -0400

    fix tests

--- tests/test_cmd_tasks.py
+++ tests/test_cmd_tasks.py
@@ -50,12 +50,8 @@ def test_cmd_alias_env_var(run_poe_subproc):
 
 def test_cmd_task_with_multiple_value_arg(run_poe_subproc, is_windows):
     result = run_poe_subproc("multiple-value-arg", "hey", "1", "2", "3", project="cmds")
-    if is_windows:
-        assert result.capture == 'Poe => poe_test_echo "first: hey second: 1 2 3"\n'
-        assert result.stdout == '"first: hey second: 1 2 3"\n'
-    else:
-        assert result.capture == "Poe => poe_test_echo first: hey second: 1 2 3\n"
-        assert result.stdout == "first: hey second: 1 2 3\n"
+    assert result.capture == "Poe => poe_test_echo first: hey second: 1 2 3\n"
+    assert result.stdout == "first: hey second: 1 2 3\n"
     assert result.stderr == ""
 
 
