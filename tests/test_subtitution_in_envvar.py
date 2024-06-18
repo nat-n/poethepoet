8af5422b4ee70a71904cf0a0bec078ffdad22c7d
D. Bohdan <dbohdan@dbohdan.com>
2024-06-18 21:24:22 +0000

    Add failing test for #223

---
+++ tests/test_subtitution_in_envvar.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+def test_substitution_in_envvar(run_poe_subproc):
+    result = run_poe_subproc("frobnicate", project="substitution_in_envvar")
+
+    assert result.capture == "Poe => ${FILE}\n"
+    assert result.stdout == f"/foo/bar/baz\n"
+    assert result.stderr == ""
