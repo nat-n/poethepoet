37766183e1b6e8272f42c9566a726f0afead0558
Nat Noordanus <n@natn.me>
2024-06-25 23:16:46 +0200

    Fix env substitution

--- tests/test_subtitution_in_envvar.py
+++
@@ -1,6 +0,0 @@
-def test_substitution_in_envvar(run_poe_subproc):
-    result = run_poe_subproc("frobnicate", project="substitution_in_envvar")
-
-    assert result.capture == "Poe => ${FILE}\n"
-    assert result.stdout == "/foo/bar/baz\n"
-    assert result.stderr == ""
