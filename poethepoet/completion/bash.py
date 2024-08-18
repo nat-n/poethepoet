c90f8a74ca68ce61bcced7ba4aff0a3db4ddf3ac
Nat Noordanus <n@natn.me>
2024-08-18 21:40:26 +0200

    Document support for global tasks and improve completion scripts
    Completion scripts and list_tasks are not configurable to respect the -C option

--- poethepoet/completion/bash.py
+++ poethepoet/completion/bash.py
@@ -1,4 +1,4 @@
-def get_bash_completion_script() -> str:
+def get_bash_completion_script(name: str = "", target_path: str = "") -> str:
     """
     A special task accessible via `poe _bash_completion` that prints a basic bash
     completion script for the presently available poe tasks
@@ -7,14 +7,18 @@ def get_bash_completion_script() -> str:
     # TODO: see if it's possible to support completion of global options anywhere as
     #       nicely as for zsh
 
+    name = name or "poe"
+    func_name = f"_{name}_complete"
+
     return "\n".join(
         (
-            "_poe_complete() {",
+            func_name + "() {",
             "    local cur",
             '    cur="${COMP_WORDS[COMP_CWORD]}"',
-            '    COMPREPLY=( $(compgen -W "$(poe _list_tasks)" -- ${cur}) )',
+            f"    COMPREPLY=( $(compgen -W \"$(poe _list_tasks '{target_path}')\""
+            + " -- ${cur}) )",
             "    return 0",
             "}",
-            "complete -o default -F _poe_complete poe",
+            f"complete -o default -F {func_name} {name}",
         )
     )
--- poethepoet/completion/fish.py
+++ poethepoet/completion/fish.py
@@ -1,4 +1,4 @@
-def get_fish_completion_script() -> str:
+def get_fish_completion_script(name: str = "") -> str:
     """
     A special task accessible via `poe _fish_completion` that prints a basic fish
     completion script for the presently available poe tasks
@@ -8,16 +8,28 @@ def get_fish_completion_script() -> str:
     # - support completion of global options (with help) only if no task provided
     #   without having to call poe for every option which would be too slow
     # - include task help in (dynamic) task completions
-    # - maybe just use python for the whole of the __list_poe_tasks logic?
+
+    name = name or "poe"
+    func_name = f"__list_{name}_tasks"
 
     return "\n".join(
         (
-            "function __list_poe_tasks",
+            "function " + func_name,
+            "    # Check if `-C target_path` have been provided",
+            "    set target_path ''",
             "    set prev_args (commandline -pco)",
-            '    set tasks (poe _list_tasks | string split " ")',
+            "    for i in (seq (math (count $prev_args) - 1))",
+            "        set j (math $i + 1)",
+            "        set k (math $i + 2)",
+            '        if test "$prev_args[$j]" = "-C" && test "$prev_args[$k]" != ""',
+            '            set target_path "$prev_args[$k]"',
+            "            break",
+            "        end",
+            "    end",
+            "    set tasks (poe _list_tasks $target_path | string split ' ')",
             "    set arg (commandline -ct)",
             "    for task in $tasks",
-            '        if test "$task" != poe && contains $task $prev_args',
+            f'        if test "$task" != {name} && contains $task $prev_args',
             # TODO: offer $task specific options
             '            complete -C "ls $arg"',
             "            return 0",
@@ -27,6 +39,6 @@ def get_fish_completion_script() -> str:
             "        echo $task",
             "    end",
             "end",
-            "complete -c poe --no-files -a '(__list_poe_tasks)'",
+            f"complete -c {name} --no-files -a '({func_name})'",
         )
     )
