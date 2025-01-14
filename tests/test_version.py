3bd0fdecfaccb78575bbe3dd41a4d8ad7692b197
Nat Noordanus <n@noordan.us>
2025-01-14 22:53:35 +0100

    fix: failing test due to pyproject.toml restructuring

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,5 +3,5 @@ from poethepoet import __version__
 
 def test_version(pyproject):
     assert (
-        __version__ == pyproject["tool"]["poetry"]["version"]
+        __version__ == pyproject["project"]["version"]
     ), "Project version should match in package and package config"
